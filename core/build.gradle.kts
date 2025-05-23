2345da3943d4348638a9025a15059dd921f1e991
Sadness <rosalimavictor@gmail.com>
2025-05-23 03:18:27 -0300

    Add initial project setup with Gradle and IDE configuration files

---
+++ core/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.minestom:minestom-snapshots:f62abc722f")
+    implementation("org.hibernate.orm:hibernate-core:7.0.0.Final")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "GameServer"
+include("core")
